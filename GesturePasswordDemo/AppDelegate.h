65dc9491ff2234f6b940e379bf57319d1f2d52f1
yicone <yicone@gmail.com>
2016-04-29 20:14:38 +0800

    Initial Commit

---
+++ GesturePasswordDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GesturePasswordDemo
+//
+//  Created by apple on 4/29/16.
+//  Copyright © 2016 PPF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GesturePasswordDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GesturePasswordDemo
+//
+//  Created by apple on 4/29/16.
+//  Copyright © 2016 PPF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
